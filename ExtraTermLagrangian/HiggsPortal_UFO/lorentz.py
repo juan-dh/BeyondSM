b03e7acc5a633dce398b1cd71ae7ff67a0f230d6
juan-dh <harodiego96@gmail.com>
2025-04-08 20:41:49 -0500

    Testeando el modelo estandar y un nuevo término

---
+++ ExtraTermLagrangian/HiggsPortal_UFO/lorentz.py
@@ -0,0 +1,14 @@
+# This file was automatically created by FeynRules 2.3.49
+# Mathematica version: 14.2.0 for Mac OS X ARM (64-bit) (December 26, 2024)
+# Date: Tue 8 Apr 2025 20:30:32
+
+
+from object_library import all_lorentz, Lorentz
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+try:
+   import form_factors as ForFac 
+except ImportError:
+   pass
+
+
